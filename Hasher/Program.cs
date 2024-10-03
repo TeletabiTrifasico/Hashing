dc0ad666464ebd061ad6b90e72ea5a422bd768ff
TeletabiTrifasico <hugojiba@gmail.com>
2024-10-03 22:55:27 +0200

    xd

---
+++ Hasher/Program.cs
@@ -0,0 +1,8 @@
+﻿public static class Hasher
+{
+    public static string HashPassword(string password)
+    {
+        // Hash the password with a salt
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+}
