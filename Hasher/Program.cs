1e5de962bc10477943cc75f955d494c160c8b446
Hugo Jiménez Barrasa <hugojiba@gmail.com>
2024-10-03 22:56:24 +0200

    Delete Hasher directory

--- Hasher/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿public static class Hasher
-{
-    public static string HashPassword(string password)
-    {
-        // Hash the password with a salt
-        return BCrypt.Net.BCrypt.HashPassword(password);
-    }
-}
